739460bfd235ac6065eec7bf23a264a3a8477725
Tien Nguyen <tien.nguyen@intrepid.asia>
2025-07-26 20:30:56 +0700

    vue test app

--- src/components/DsButton.vue
+++ src/components/DsButton.vue
@@ -2,13 +2,9 @@
   <button class="ds-button"><slot /></button>
 </template>
 
-<!--
+
 <style scoped>
 .ds-button {
-  background-color: var(&#45;&#45;primary-color, #2563eb);
-  font-family: var(&#45;&#45;font-family, sans-serif);
-  border-radius: var(&#45;&#45;border-radius, 4px);
-  color: white;
-  padding: 0.5rem 1rem;
+  background-color: #42b883;
 }
-</style>-->
+</style>

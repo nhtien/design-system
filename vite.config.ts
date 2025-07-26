53fd653192c5c91bc891c1fcee5ec70b9fb40a6d
Tien Nguyen <tien.nguyen@intrepid.asia>
2025-07-26 20:33:55 +0700

    vue test app

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,10 @@ import vue from '@vitejs/plugin-vue';
 import dts from 'vite-plugin-dts';
 
 export default defineConfig({
-    plugins: [vue(), dts()],
+    plugins: [vue(), dts({
+        insertTypesEntry: true
+    })
+    ],
     build: {
         lib: {
             entry: 'src/index.ts',

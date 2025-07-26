effc82021545a280579b9e46d6ce0e3294096ae5
Tien Nguyen <tien.nguyen@intrepid.asia>
2025-07-26 20:53:40 +0700

    build app

---
+++ dist/index.d.ts
@@ -0,0 +1,2 @@
+import { default as DesignSystemPlugin } from './plugin/design-system';
+export default DesignSystemPlugin;
---
+++ dist/plugin/design-system.d.ts
@@ -0,0 +1,14 @@
+import { App } from 'vue';
+export declare const DS_THEME_KEY: unique symbol;
+export interface DsTheme {
+    fontFamily?: string;
+    primaryColor?: string;
+    borderRadius?: string;
+    textColor?: string;
+    className?: string;
+    [key: string]: string | undefined;
+}
+declare const _default: {
+    install(app: App, config: DsTheme): void;
+};
+export default _default;

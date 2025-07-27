e61e8c81b8351092401b4fda383e7258a22ae8aa
Tien Nguyen <tien.nguyen@intrepid.asia>
2025-07-27 14:52:03 +0700

    build 4

--- dist/my-design-system.es.js
+++ dist/my-design-system.es.js
@@ -1,17 +1,17 @@
 (function(){"use strict";try{if(typeof document<"u"){var t=document.createElement("style");t.appendChild(document.createTextNode('.ds-scope-intrepid{font-family:var(--font-family, "Inter", sans-serif)!important;color:var(--text-color, #1f2937)!important;display:inline-block!important}.ds-button[data-v-ac2d53c8]{background-color:#42b883}')),document.head.appendChild(t)}}catch(e){console.error("vite-plugin-css-injected-by-js",e)}})();
-import { createElementBlock as _, openBlock as b, renderSlot as h, defineComponent as D, inject as g, computed as p, h as l, reactive as j } from "vue";
-const v = (t, e) => {
+import { createElementBlock as _, openBlock as b, renderSlot as h, defineComponent as D, inject as g, computed as u, h as l, reactive as j } from "vue";
+const y = (t, e) => {
   const o = t.__vccOpts || t;
   for (const [s, n] of e)
     o[s] = n;
   return o;
 }, C = {}, O = { class: "ds-button" };
-function y(t, e) {
+function v(t, e) {
   return b(), _("button", O, [
     h(t.$slots, "default", {}, void 0, !0)
   ]);
 }
-const w = /* @__PURE__ */ v(C, [["render", y], ["__scopeId", "data-v-ac2d53c8"]]), f = /* @__PURE__ */ Object.freeze(/* @__PURE__ */ Object.defineProperty({
+const w = /* @__PURE__ */ y(C, [["render", v], ["__scopeId", "data-v-ac2d53c8"]]), f = /* @__PURE__ */ Object.freeze(/* @__PURE__ */ Object.defineProperty({
   __proto__: null,
   DsButton: w
 }, Symbol.toStringTag, { value: "Module" })), m = Symbol("DsThemeConfig");
@@ -19,18 +19,17 @@ function d(t) {
   return D({
     name: `DsWrapper(${t.name ?? "Anonymous"})`,
     inheritAttrs: !1,
-    // Prevent passing attributes to outer <div> unnecessarily
     setup(e, { slots: o, attrs: s }) {
-      const n = g(m), r = p(() => {
+      const n = g(m), r = u(() => {
         const a = {};
-        return n && Object.entries(n).forEach(([i, u]) => {
-          u && i !== "className" && (a[`--${B(i)}`] = u);
-        }), a;
-      }), c = p(
+        return n && Object.entries(n).forEach(([i, p]) => {
+          p && i !== "className" && (a[`--${B(i)}`] = p);
+        }), a.display = "contents", a;
+      }), c = u(
         () => (n == null ? void 0 : n.className) || "ds-scope-intrepid"
       );
       return () => l(
-        "div",
+        "span",
         { class: c.value, style: r.value },
         [l(t, { ...s }, o)]
       );
